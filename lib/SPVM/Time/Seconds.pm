9d4ea515b63c670c7fe8eae72f35b8f183c0b764
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-12-07 16:04:28 +0900

    Add empty Time::Seconds.

---
+++ lib/SPVM/Time/Seconds.pm
@@ -0,0 +1,45 @@
+package SPVM::Time::Seconds;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::Time::Seconds - Short Description
+
+=head1 Description
+
+The Time::Seconds class of L<SPVM> has methods to do someting.
+
+=head1 Usage
+
+  use Time::Seconds;
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+[--user-name] C<[--user-email]>
+
+=head1 Copyright & License
+
+Copyright (c) 2023 [--user-name]
+
+MIT License
+
